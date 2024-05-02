9b3b194c396c824663b07cae4b552e32c2c66f1f
andrei <andrei@andreis-MacBook-Air.local>
2024-05-02 13:09:38 +0300

    feat: add style chart

--- assets/js/statistics-page.js
+++ assets/js/statistics-page.js
@@ -1,25 +1,11 @@
 initCustomSelect();
 
-var options = {
-	chart: {
-		type: 'area',
-		width: '100%',
-		height: '100%',
-		zoom: {
-			enabled: false,
-		},
-		toolbar: {
-			show: false,
-		},
-	},
-	series: [
-		{
-			name: 'Hashrate',
-			data: [30, 33, 29, 20, 28, 17, 18, 29, 30],
-		},
-	],
-	xaxis: {
-		categories: [
+const CHART_HASH_RATE = document.querySelector('#chartYourHashrate');
+const CHARTS_HISTORY_CELL_TABLE = document.querySelectorAll('#historyChart');
+const CHART_BASE_OPTIONS = {
+	type: 'line',
+	data: {
+		labels: [
 			'Value 1',
 			'Value 2',
 			'Value 3',
@@ -30,92 +16,152 @@ var options = {
 			'Value 8',
 			'Value 9',
 		],
-
-		axisBorder: {
-			show: true,
-			color: '#AFB8BF',
-			height: 1,
-			offsetX: 0,
-			offsetY: 0,
-		},
-
-		axisTicks: {
-			show: false,
-		},
+		datasets: [
+			{
+				label: 'Hashrate',
+				data: [30, 33, 29, 20, 28, 17, 18, 29, 30],
+				backgroundColor: 'rgba(155, 77, 202, 0.24)',
+				borderColor: '#9B4DCA',
+				borderWidth: 2,
+				fill: true,
+				pointRadius: 0,
+			},
+		],
 	},
-
-	yaxis: {
-		show: true,
-		showAlways: true,
-		tickAmount: 4,
-		min: 0,
-		max: 40,
-		axisBorder: {
-			show: true,
-			color: '#AFB8BF',
-			offsetX: 0,
-			offsetY: 0,
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+				min: 0,
+				max: 40,
+				ticks: {
+					stepSize: 10,
+				},
+				grid: {
+					display: false,
+				},
+			},
+			x: {
+				grid: {
+					display: false,
+				},
+			},
 		},
-
-		title: {
-			text: 'H/S',
-			rotate: 0,
-			offsetX: 20,
-			offsetY: -130,
-			style: {
-				color: undefined,
-				cssClass: 'apexcharts-yaxis-title',
+		plugins: {
+			legend: {
+				display: false,
+			},
+			tooltip: {
+				enabled: false,
+			},
+			title: {
+				display: true,
+				text: 'EH/s',
+				font: {
+					size: 14,
+				},
+				color: '#606C76',
+				position: 'top',
+				align: 'start',
+				font: { weight: 'normal' },
+			},
+		},
+		responsive: true,
+		maintainAspectRatio: false,
+	},
+};
+const CHART_HISTORY_CELL_TABLE_OPTIONS = getChartOptions({
+	options: {
+		scales: {
+			y: {
+				ticks: {
+					display: false,
+				},
+				grid: {
+					display: false,
+				},
+			},
+			x: {
+				ticks: {
+					display: false,
+				},
+				grid: {
+					display: false,
+				},
+			},
+		},
+		plugins: {
+			legend: {
+				display: false,
+			},
+			tooltip: {
+				enabled: false,
+			},
+			title: {
+				display: false,
 			},
 		},
 	},
-
-	stroke: {
-		curve: 'smooth',
-		width: 2,
-		colors: ['#9B4DCA'],
-	},
-
-	fill: {
-		colors: ['rgba(155, 77, 202, 0.24)', 'rgba(155, 77, 202, 0)'],
-		type: 'gradient',
-		gradient: {
-			shade: 'light',
-			type: 'vertical',
-			shadeIntensity: 1,
-			opacityFrom: 0.7,
-			opacityTo: 1,
-			stops: [0, 90, 100],
-			colorStops: [],
-		},
-	},
-
-	tooltip: {
-		enabled: false,
-	},
-
-	dataLabels: {
-		enabled: false,
-	},
-};
-
-var chart = new ApexCharts(document.querySelector('#chart'), options);
-chart.render();
-
-function updateChartData(data, label) {
-	myChart.data.datasets[0].data = data;
-	myChart.data.datasets[0].label = label;
-	myChart.update();
-}
-
-document.querySelector('.hourButton').addEventListener('click', function () {
-	updateChartData([25, 26, 27, 30, 29, 28, 30, 32, 31], '1 Hour');
 });
 
-document.querySelector('.dayButton').addEventListener('click', function () {
-	updateChartData([22, 23, 24, 26, 25, 25, 27, 28, 26], '24 Hours');
+function initializeChart(chartElement, chartOptions) {
+	const ctx = chartElement.getContext('2d');
+	const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+	gradient.addColorStop(0, 'rgba(155, 77, 202, 0.24)');
+	gradient.addColorStop(1, 'rgba(155, 77, 202, 0)');
+
+	chartOptions.data.datasets.forEach(dataset => {
+		dataset.backgroundColor = gradient;
+	});
+
+	let chart = new Chart(ctx, chartOptions);
+	return chart;
+}
+
+function deepMerge(target, source) {
+	Object.keys(source).forEach(key => {
+		if (source[key] && typeof source[key] === 'object') {
+			target[key] = target[key] || (Array.isArray(source[key]) ? [] : {});
+			deepMerge(target[key], source[key]);
+		} else {
+			target[key] = source[key];
+		}
+	});
+	return target;
+}
+
+function getChartOptions(newOptions) {
+	if (!newOptions) {
+		return CHART_BASE_OPTIONS;
+	}
+	const options = JSON.parse(JSON.stringify(CHART_BASE_OPTIONS));
+	return deepMerge(options, newOptions);
+}
+
+const hashRateChart = initializeChart(CHART_HASH_RATE, getChartOptions());
+
+CHARTS_HISTORY_CELL_TABLE.forEach(chart => {
+	initializeChart(chart, getChartOptions(CHART_HISTORY_CELL_TABLE_OPTIONS));
+});
+
+function updateChartData(chart, newData) {
+	chart.data.datasets[0].data = newData;
+	chart.update();
+}
+
+document.querySelector('.hourButton').addEventListener('click', function (e) {
+	updateChartData(hashRateChart, [25, 26, 27, 30, 29, 28, 30, 32, 31]);
+});
+
+document.querySelector('.dayButton').addEventListener('click', function (e) {
+	updateChartData(hashRateChart, [22, 23, 24, 26, 25, 25, 27, 28, 26]);
 });
 
 const tabsWorkersAndPayouts = document
+	.querySelector('.chart-interval__workers-payouts-container')
+	.querySelectorAll('.tab');
+
+const tabsIntervalHourAndDay = document
 	.querySelector('.chart-interval__workers-payouts-container')
 	.querySelectorAll('.tab');
 
@@ -144,3 +190,5 @@ modal.addEventListener('click', e => {
 		modal.close();
 	}
 });
+
+// var ctx = document.getElementById('chart').getContext('2d');
