c9608fbc3fc74467a39dfc01c53abece654c57a4
andrei <andrei@andreis-MacBook-Air.local>
2024-05-09 15:58:43 +0300

    feat: add show profit to usd

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,34 +4,17 @@ const LANGUAGE_SWITCHER_ELEMENTS = document.querySelectorAll(
 const HAMBURGER_MENU_BTN = document.querySelector('.hamburger-menu-btn');
 const statsApiUrl = 'https://api.coinmore.io';
 
-LANGUAGE_SWITCHER_ELEMENTS.forEach(el =>
-	el.addEventListener('click', e => {
-		e.stopPropagation();
-		e.currentTarget.classList.toggle('open');
-	})
-);
-
-HAMBURGER_MENU_BTN.addEventListener('click', function (e) {
-	e.currentTarget.classList.toggle('active');
-});
-
-document.body.addEventListener('click', () => {
-	LANGUAGE_SWITCHER_ELEMENTS.forEach(e => {
-		e.classList.remove('open');
-	});
-});
-
 function statsApiCall(action) {
 	return fetch(`${statsApiUrl}${action}`).then(response => response.json());
 }
 
-// function fetchCurrencyInfo(coin) {
-// 	return statsApiCall(`/rate?coin=alephium`);
-// }
+function fetchCurrencyInfo(coin) {
+	return statsApiCall(`/rate?coin=alephium`);
+}
 
-// function fetchPoolProfit(coin) {
-// 	return statsApiCall('/profit?coin=alephium');
-// }
+function fetchPoolProfit(coin) {
+	return statsApiCall('/profit?coin=alephium');
+}
 
 function fetchPoolHashRate(coin) {
 	return statsApiCall('/hashrate?coin=alephium');
@@ -98,13 +81,20 @@ function showRate(rate) {
 	document.getElementById('coin-price').textContent = `$${rate.value}`;
 }
 
+function showPoolProfitUSD(rate, profit) {
+	const floatProfit = parseFloat(profit);
+	const floatRate = parseFloat(rate);
+	const profitUSD = (floatProfit * floatRate).toFixed(4);
+
+	document.getElementById('pool_profit_usd').textContent = `$${profitUSD}`;
+}
+
 function init() {
-	// fetchPoolProfit().then(({ profit }) => {
-	// 	// showPoolProfit(profit);
-	// 	// fetchCurrencyInfo().then(({ rate: { value } }) =>
-	// 	// 	showPoolProfitUSD(profit, value)
-	// 	// );
-	// });
+	fetchPoolProfit().then(({ profit }) => {
+		fetchCurrencyInfo().then(({ rate: { value } }) =>
+			showPoolProfitUSD(profit, value)
+		);
+	});
 
 	fetchPoolHashRate().then(({ hashrate }) => {
 		showPoolHashrate(hashrate.hashrate);
@@ -121,36 +111,23 @@ function init() {
 	fetchRate().then(({ rate }) => {
 		showRate(rate);
 	});
-
-	// fetchPoolBlocks(86400).then(({ last_block_at, count }) => {
-	// 	// showPoolLatestBlockAt(last_block_at);
-	// 	// showPool24hBlocks(count);
-	// });
 }
 
 init();
 
-// function showPoolProfit(profit) {
-// 	const roundProfit = parseFloat(profit).toFixed(4);
-// 	document.getElementById('pool_profit').textContent = `${roundProfit}`;
-// }
+LANGUAGE_SWITCHER_ELEMENTS.forEach(el =>
+	el.addEventListener('click', e => {
+		e.stopPropagation();
+		e.currentTarget.classList.toggle('open');
+	})
+);
 
-// function showPoolProfitUSD(rate, profit) {
-// 	const floatProfit = parseFloat(profit);
-// 	const floatRate = parseFloat(rate);
-// 	const profitUSD = (floatProfit * floatRate).toFixed(4);
+HAMBURGER_MENU_BTN.addEventListener('click', e => {
+	e.currentTarget.classList.toggle('active');
+});
 
-// 	document.getElementById('pool_profit_usd').textContent = `${profitUSD}`;
-// }
-
-// function showPool24hBlocks(blocksCount) {
-// 	document.getElementById('24h_blocks').textContent = blocksCount;
-// }
-
-// function showPoolLatestBlockAt(date) {
-// 	const current = new Date();
-// 	const at = new Date(date);
-// 	const hours = (Math.abs(current - at) / 36e5).toFixed(2);
-
-// 	document.getElementById('latest_block_at').textContent = `${hours} hour(s)`;
-// }
+document.body.addEventListener('click', () => {
+	LANGUAGE_SWITCHER_ELEMENTS.forEach(e => {
+		e.classList.remove('open');
+	});
+});
