883d5ba16640a320138ad71c3c929b98baca2ee8
mtshdev <andrey.matsiash@gmail.com>
2024-07-16 15:48:40 +0300

    feat: redirect to page with desired language

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const LANGUAGE_SWITCHER_ELEMENTS =
   document.querySelectorAll('.language-switcher');
 const HAMBURGER_MENU_BTN = document.querySelector('.hamburger-menu-btn');
+const LINK_SET_LANG_EN = document.querySelector('.set-lang-english')
+const LINK_SET_LANG_RU = document.querySelector('.set-lang-russian')
 
 LANGUAGE_SWITCHER_ELEMENTS.forEach((el) =>
   el.addEventListener('click', (e) => {
@@ -20,3 +22,33 @@ document.body.addEventListener('click', () => {
   });
   HAMBURGER_MENU_BTN.classList.remove('active');
 });
+
+
+let currentPath = window.location.pathname;
+let userLang = localStorage.getItem('userLang');
+
+if (!userLang) {
+  let systemLang = navigator.language || navigator.userLanguage;
+  let lang = systemLang.startsWith('ru') ? 'ru' : 'en';
+
+  if (!currentPath.includes(lang)) {
+    let newPath = `/${lang}/${currentPath}`
+    window.location.href = newPath;
+  }
+}
+
+function setUserLanguage(lang) {
+  localStorage.setItem('userLang', lang);
+  let newPath = `/${lang}/${currentPath}`
+  window.location.href = newPath;
+}
+
+
+LINK_SET_LANG_EN.addEventListener('click', () => {
+    setUserLanguage('en');
+  });
+
+
+LINK_SET_LANG_RU.addEventListener('click', () => {
+    setUserLanguage('ru');
+  });
